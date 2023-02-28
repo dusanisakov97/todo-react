8906e23e267ce4b1937c8112f71db140616441f8
dusanisakov97 <dusisakov@gmail.com>
2023-02-28 21:39:29 +0100

    added icons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,6 +6,12 @@ const Todo = ({ index, todo, deleteTodo }) => {
       <li key={index} className="todo-item">
         {todo}
       </li>
+      <button className="complete-btn">
+        <i class="fas fa-check"></i>
+      </button>
+      <button className="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
       <button className="trash-btn" onClick={() => deleteTodo(index)}>
         <i class="fas fa-trash"></i>
       </button>
